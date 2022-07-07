93bf66062865eb4e117a9831f2a3557350a2740e
kunal-mrk <108858478+kunal-mrk@users.noreply.github.com>
2022-07-06 23:47:04 -0700

    Added my 7th july file
    Added my 7th july file

---
+++ 7th july.js
@@ -0,0 +1,13 @@
+function Hello(){
+	var a=document.getElementById('t1').value;
+	var b=2*a;
+	console.log(b);
+	
+}
+
+function Clean(){
+	var c=document.getElementById('t1').value;
+	var d=2*c;
+	document.getElementById('result').textContent='Result is '+d;
+	
+}
