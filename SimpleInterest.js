0a1998e09a53ffcabf96f06defe2b6130a07d393
kunal-mrk <108858478+kunal-mrk@users.noreply.github.com>
2022-07-07 07:37:44 -0700

    Simple Interest Project
    Simple Interest Project

---
+++ SimpleInterest.js
@@ -0,0 +1,8 @@
+function Interest(){
+	var a=document.getElementById('name1').value;
+	var b=document.getElementById('name2').value;
+	var c=document.getElementById('name3').value;
+	var sum=parseInt(a)*parseInt(b)*parseInt(c)/100;
+	console.log(sum);
+	
+}
