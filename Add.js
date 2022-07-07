74c4f2e072e7e176400db9986ba83334aaeb0ae6
kunal-mrk <108858478+kunal-mrk@users.noreply.github.com>
2022-07-07 07:31:20 -0700

    Upload sum,sub,Mul,div project
    Upload sum,sub,Mul,div project

---
+++ Add.js
@@ -0,0 +1,24 @@
+function Hello(){
+	var a=document.getElementById('t1').value;
+	var b=document.getElementById('t2').value;
+	var sum=parseInt(a)+parseInt(b);
+	console.log(sum);
+}
+
+function substract(){
+	var c = document.getElementById('t1').value;
+	var d=document.getElementById('t2').value;
+	console.log(c-d);
+}
+
+function multiply(){
+	var e=document.getElementById('t1').value;
+	var f=document.getElementById('t2').value;
+	console.log(e*f);
+}
+
+function Division(){
+	var g=document.getElementById('t1').value;
+	var h=document.getElementById('t2').value;
+	console.log(g/h);
+}
