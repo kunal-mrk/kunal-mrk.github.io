8cd5380416a4c3f3cd2ec63ea79723cd9e8119ea
kunal-mrk <108858478+kunal-mrk@users.noreply.github.com>
2022-07-07 00:29:21 -0700

    Added Area and Perimeter of circle
    Added Area and Perimeter of circle

---
+++ Area.js
@@ -0,0 +1,9 @@
+function Hello(){
+	var radius=document.getElementById('r').value;
+	var Area=3.14*(radius*radius);
+	var perimeter=2*3.14*radius;
+	console.log('Area of Circle = '+ Area);
+	console.log('Perimeter of Circle = '+ perimeter);
+	
+	}
+	
