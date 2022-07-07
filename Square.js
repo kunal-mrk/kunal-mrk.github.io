04537d9f3adbbe77740d5483a25e607e06db1d43
kunal-mrk <108858478+kunal-mrk@users.noreply.github.com>
2022-07-07 00:35:08 -0700

    Add files via upload

---
+++ Square.js
@@ -0,0 +1,8 @@
+function Hello(){
+	var s=document.getElementById('side').value;
+	var Area=s*s;
+	var perimeter=4*s;
+	console.log('Area of square ='+Area);
+	console.log('Perimeter of square ='+perimeter);
+  
+}
